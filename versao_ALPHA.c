586b07c45af387b73e1b6536498a81358cd67c67
isabella738 <isabellasportof@gmail.com>
2026-06-19 06:28:42 -0300

    Atualizar o versao_ALPHA.c

--- versao_ALPHA.c
+++ versao_ALPHA.c
@@ -5,7 +5,6 @@
 LISTA DE AFAZERES (que ironia)
 - Criar funcoes de leitura e verificacao
 - Fazer as funcionalidades solicitadas pelo projeto
-- Requisito 1: Pensar numa forma de acrescentar tarefas ao mesmo tempo que as realoca por prioridade
 */
 
 typedef struct {
